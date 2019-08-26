bdcf9196ccf7ae213a103fc2a02f482a8d1bbebb
Herri Fransisca <herrifransisca@gmail.com>
2019-08-26 14:11:46 +0700

    fix typo

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 
 import rootReducer from './root-reducer';
