c584a29ea9c78c1e16aab09e171f6337554486f3
Herri Fransisca <herrifransisca@gmail.com>
2019-09-28 00:25:26 +0700

    fixed bug

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -2,17 +2,17 @@ import { createSelector } from 'reselect';
 
 const selectCart = state => state.cart;
 
-export const selectCartItems = createSelector([
+export const selectCartItems = createSelector(
   [selectCart],
   cart => cart.cartItems
-]);
+);
 
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems =>
     cartItems.reduce(
-      (accumulatedQuantity, cartItem) =>
-        accumulatedQuantity + cartItem.quantity,
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
       0
     )
 );
