e33276aa1524d6368d25cfe7b9713ff725f46697
Herri Fransisca <herrifransisca@gmail.com>
2019-12-07 21:14:27 +0700

    2- Promise Pattern- implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. also included withSpinner HOC

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -28,11 +28,17 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+    collectionRef.get().then(async snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
     });
+
+    // fetch(
+    //   'https://firestore.googleapis.com/v1/projects/crwn-clothing-4a55b/databases/(default)/documents/collections'
+    // )
+    //   .then(response => response.json())
+    //   .then(collections => console.log(collections));
   }
 
   render() {
